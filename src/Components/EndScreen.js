165294e35bb0f0d613489d724465b0b475107c95
KalidindiPoojaSahithi <poojasahithi82@gmail.com>
2024-06-22 18:36:25 -0400

    Initial commit

---
+++ src/Components/EndScreen.js
@@ -0,0 +1,21 @@
+import React, {useContext} from 'react';
+import { QuizContext } from '../Helpers/Context';
+import { Questions } from '../Helpers/QuestionBank';
+import "../App.css";
+function EndScreen() {
+  const {score, setScore, setGameState} = useContext(QuizContext);
+  const restartQuiz = () => {
+    setScore(0);
+    setGameState("menu");
+  }
+  return (
+    <div className='EndScreen'>
+      <h1>Quiz Finished!</h1>
+      <h3> You have scored {score} out of {Questions.length} </h3>
+      <button onClick = {restartQuiz}> Restart Quiz</button>
+
+    </div>
+  )
+}
+
+export default EndScreen
---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,25 @@
+export const Questions = [
+    {
+        prompt: "What is the capital of New Jersey",
+        optionA: "Jersey city",
+        optionB: "Trenton",
+        optionC: "Newark",
+        optionD: "Atlantic City",
+        answer: "B",
+    },
+    {
+        prompt: "What is a Streaming Platform",
+        optionA: "Twitch",
+        optionB: "Google",
+        optionC: "Instagram",
+        optionD: "WhatsApp",
+        answer: "A",
+    },{
+        prompt: "Who is the current president of USA",
+        optionA: "Donald Trump",
+        optionB: "Barack Obama",
+        optionC: "Bill Clinton",
+        optionD: "Joe Biden",
+        answer: "D",
+    },
+];
